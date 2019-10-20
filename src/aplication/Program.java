9db585ee57eebb05ab4f11d1b96f2d374dbda30e
Murilo Fenga <mfenga@yahoo.com.br>
2019-10-20 10:44:19 -0300

    teste

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,9 +1,16 @@
 package aplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
 
 	}
 
